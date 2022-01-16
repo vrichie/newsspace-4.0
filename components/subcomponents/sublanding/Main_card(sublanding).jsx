1578ddac585dc37e41513c06b0a672fb4f07b625
vrichie <vrichieking@gmail.com>
2022-01-16 12:29:18 +0300

    16.01

--- components/subcomponents/sublanding/Main_card(sublanding).jsx
+++ components/subcomponents/sublanding/Main_card(sublanding).jsx
@@ -48,7 +48,7 @@ export default function Main_card() {
 
     return (
         
-        <Link href={`/article/${post.slug}`} key={}>
+        <Link href={`/article/${post.slug}`} >
             <div id={style.content}>
 
               <div id={style.main_image_content}>
